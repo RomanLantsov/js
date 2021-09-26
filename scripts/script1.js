1cf936d2e202f50c6c3044cf0932c856e4fba478
RomanLantsov <skvirl@gmail.com>
2021-09-26 23:48:24 +0300

    save changes

--- scripts/script1.js
+++ scripts/script1.js
@@ -2,8 +2,12 @@
 
  
  
-let a = +prompt("Первое число?", 1);
-let b = +prompt("Второе число?", 2);
+// let a = +prompt("Первое число?", 1);
+// let b = +prompt("Второе число?", 2);
 
- 
- document.getElementById("output1").innerHTML =  a +   b;
+let a = 0;
+let b = '0';
+
+
+
+ document.getElementById("output1").innerHTML =  (a) == (b);
