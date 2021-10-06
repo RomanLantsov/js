f4625f20731557e492c7e2f29739c85c84f591ec
RomanLantsov <skvirl@gmail.com>
2021-10-06 21:51:14 +0300

    newFeachures_061021

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,13 +1,45 @@
 "use strict";
 
- 
- 
-// let a = +prompt("Первое число?", 1);
-// let b = +prompt("Второе число?", 2);
+// let login, password;
+// login = prompt("Логин: ");
+// if (login === null | login === '') alert("Отменено");
+// else if (login.toLowerCase() === "админ" ) {
+//   password = prompt("Пароль: ");
+//   if (password === null || password ==='') alert("Отменено");
+//   else if ((password.toLowerCase() ) === "я главный") alert("Здравствуйте!");
+//   else alert("Неверный пароль");
+// } else alert("Я вас не знаю");
 
-let a = 0;
-let b = '0';
+// for (let index = 2; index <= 10 ; index++) {
+//   if (index%2!=0) continue
+//   console.log(index)
+// }
+
+// let i = 0;
+// while(i<3) alert(`number ${i++}`)
+
+// // простые числа!!
+
+// let n = 10000;
+
+// i_cycle: for (let i = 1; i < n; i++) {
+//   let j = 1;
+//   while (++j < i) if (i % j === 0) continue i_cycle;
+//   console.log(i);
+// }
+
+// const number = +prompt('Введите число между 0 и 3', '');
+// switch(number) {
+//   case 0:  alert('Вы ввели число 0'); break;
+//   case 1:  alert('Вы ввели число 1'); break;
+//   case 2: case 3: alert('Вы ввели число 2, а может и 3');
+// }
+
+function min(a,b){
+  return a<b?a:b
+}
 
 
-
- document.getElementById("output1").innerHTML =  (a) == (b);
+console.log(`a = ${3}, b = ${1}:  ${min(3,1)}`)
+console.log(`a = ${-2}, b = ${1}:  ${min(3,1)}`)
+console.log(`a = ${1}, b = ${1}:  ${min(3,1)}`)
